903f56575ebeb2d5da6cfddcfa187e9f3bb76d15
MariusMRN <marryusg48@gmail.com>
2020-04-18 22:25:13 +0300

    Update MainActivity.java

--- app/src/main/java/com/mrn/challenge1/MainActivity.java
+++ app/src/main/java/com/mrn/challenge1/MainActivity.java
@@ -2,23 +2,25 @@ package com.mrn.challenge1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button_start, button_click;
     private TextView textView;
-    private int ClicksCount = 0;
+    private double ClicksCount = 0.0;
     public boolean running;
     public Chronometer timer;
     public double CPSresult;
+    public String result;
+    public final String CPS_FINALE = "TextContent";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +40,7 @@ public class MainActivity extends AppCompatActivity {
             public void onChronometerTick(Chronometer chronometer) {
                 if ((SystemClock.elapsedRealtime() - timer.getBase()) >= 11000){
                     timer.setBase(SystemClock.elapsedRealtime());
-                    CPSresult = ClicksCount / 10;
-                    String result = "You have " + CPSresult + " CPS";
-                    textView.setText(result);
-                    running = false;
-                    timer.stop();
-                    ClicksCount = 0;
+                    result();
                 }
             }
         });
@@ -51,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener startButtonOnClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                textView.setTextColor(Color.parseColor("#b0aeac"));
                 textView.setText("");
                 timeStart();
             }
@@ -70,11 +68,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void timeStart(){
         if (!running){
-            ClicksCount = 0;
+            ClicksCount = 0.0;
             timer.setBase(SystemClock.elapsedRealtime());
             timer.start();
             running = true;
         }
+    }
+
+    public void result(){
+        CPSresult = ClicksCount / 10;
+        if (ClicksCount <= 60) {
+            result = "Well..you are an Octopus, smart but not fast\nYou clicked with the speed of " + CPSresult + " CPS\nYou made a total of " + ClicksCount + " clicks in 10 seconds";
+        }else if (ClicksCount > 60 ){
+            textView.setTextColor(Color.parseColor("#ffffff"));
+            result = "Wow..you are a Cheetah.Hail to the king of clicking\nYou clicked with the speed of " + CPSresult + " CPS\nYou made a total of " + ClicksCount + " clicks in 10 seconds";
+        }
+        textView.setText(result);
+        running = false;
+        timer.stop();
+        ClicksCount = 0.0;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        textView.setText(savedInstanceState.getString(CPS_FINALE));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        outState.putString(CPS_FINALE, textView.getText().toString());
+        super.onSaveInstanceState(outState);
     }
 
 }
